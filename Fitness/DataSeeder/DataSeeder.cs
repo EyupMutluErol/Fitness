752a1e65d9b17cf96b6761f6627ca73e5fbc6d24
EyupMutluErol <eyupmutluerol@gmail.com>
2025-11-21 19:40:39 +0300

    Configure Dependency Injection, initialize Identity services, and integrate Admin/Role data seeding.

---
+++ Fitness/DataSeeder/DataSeeder.cs
@@ -0,0 +1,48 @@
+﻿using Fitness.Entities.Concrete;
+using Microsoft.AspNetCore.Identity;
+
+namespace Fitness.DataSeeder
+{
+    public static class DataSeeder
+    {
+        public static async Task SeedRolesAndAdminAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<AppUser>>();
+
+            string[] roleNames = {"Admin", "Member" , "Trainer"};
+            foreach (var roleName in roleNames)
+            {
+                if(!await roleManager.RoleExistsAsync(roleName))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            string adminEmail = "b231210101@sakarya.edu.tr";
+            string adminPassword = "sau";
+
+            var adminUser = await userManager.FindByEmailAsync(adminEmail);
+            if(adminUser == null)
+            {
+                var user = new AppUser
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    FirstName = "Proje",
+                    LastName = "Admin",
+                    EmailConfirmed = true,
+                    PhoneNumber = "5551234567"
+                };
+                var result = await userManager.CreateAsync(user, adminPassword);
+
+                if(result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "Admin");
+                }
+            }
+        }
+
+
+    }
+}
--- Fitness/Program.cs
+++ Fitness/Program.cs
@@ -1,30 +1,67 @@
+using Fitness.Business.Abstract;
+using Fitness.Business.Concrete;
+using Fitness.Data.Abstract;
+using Fitness.Data.Concrete;
 using Fitness.Data.Concrete.Context;
+using Fitness.DataSeeder;
+using Fitness.Entities.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    builder.Configuration.AddUserSecrets<Program>();
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<FitnessDbContext>(options =>
     options.UseSqlServer(connectionString));
 
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 3;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireUppercase = false;
+    options.SignIn.RequireConfirmedAccount = false;
+}).AddEntityFrameworkStores<FitnessDbContext>().AddDefaultTokenProviders();
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<ITrainerRepository, EfTrainerRepository>();
+builder.Services.AddScoped<IAppointmentRepository, EfAppointmentRepository>();
+builder.Services.AddScoped<IServiceRepository, EfServiceRepository>();
+builder.Services.AddScoped<IGymRepository, EfGymRepository>();
+builder.Services.AddScoped<IAvailabilityRepository, EfAvailabilityRepository>();
+
+builder.Services.AddScoped<ITrainerService,TrainerManager>();
+builder.Services.AddScoped<IAppointmentService, AppointmentManager>();
+builder.Services.AddScoped<IServiceService, ServiceManager>();
+builder.Services.AddScoped<IGymService, GymManager>();
+builder.Services.AddScoped<IAvailabilityService, AvailabilityManager>();
+
+builder.Services.AddAuthorization();
+builder.Services.AddControllersWithViews();
+
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
@@ -34,5 +71,10 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+using(var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    DataSeeder.SeedRolesAndAdminAsync(serviceProvider).Wait();
+}
 
 app.Run();
